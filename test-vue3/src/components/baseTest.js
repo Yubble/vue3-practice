6dae94c17d413e297e1a33c7f358d0ca0d620fcd
liuyanbao <liuyanbao@shuidi-inc.com>
2021-12-28 15:52:47 +0800

    feat: 提交练习

--- test-vue3/src/components/baseTest.js
+++ test-vue3/src/components/baseTest.js
@@ -4,15 +4,25 @@
  * @Author: 刘燕保
  * @Date: 2021-12-20 12:34:34
  **/
-import { ref, watch, computed, reactive } from 'vue'
+import { ref, watch, computed, reactive, isReactive, toRaw, watchEffect } from 'vue'
 
 export default function baseFun() {
   // 对外输出的count双向绑定变量
   const count = ref(0)
   console.log('count is ', count)
+  console.log('isReactive 判断 ref ', isReactive(count))
   // 测试reactive
-  const state = reactive({ state1: '111', state2: 222 })
+  const state = reactive({ state1: '111', state2: 222, child: { s1: '11' } })
   console.log('state1 is ', state.state1)
+  console.log('isReactive 判断 ', isReactive(state))
+  console.log('isReactive 判断 state.state1', isReactive(state.state1))
+  console.log('isReactive 判断子组件的响应能力 ', isReactive(state.child))
+  const raw = toRaw(state)
+  raw.state1 = 531
+  console.log('raw is ', raw)
+  const addRaw = () => {
+    raw.state1++
+  }
   // 对外输出的add增加方法
   const add = () => {
     count.value++
@@ -21,11 +31,15 @@ export default function baseFun() {
   watch(count, (newValue, oldValue) => {
     console.log('监听到了 count 的变化，oldValue is ', oldValue, ' newValue is ', newValue)
   })
+  // 首先运行一次传入的函数，同时监听响应式变量，发生变化后则运行对应函数
+  watchEffect(() => { console.log('watchEffect运行，监听到 count.value 的变化 count is ', count.value) })
   // 进行对count的2倍双向绑定计算属性
   const twiceTheCounter = computed(() => count.value * 2)
   return {
     add,
     count,
-    twiceTheCounter
+    twiceTheCounter,
+    raw,
+    addRaw
   }
 }
--- test-vue3/src/views/HelloWorld.vue
+++ test-vue3/src/views/HelloWorld.vue
@@ -7,12 +7,17 @@
 <template>
   <button @click='add'>{{ count }}</button>
   <p>双倍的count：{{ twiceTheCounter }}</p>
+
+  <p>野生的 {{ raw }}</p>
+  <button @click='addRaw'>增加野生state1数值</button>
 </template>
 
 <script>
 import baseTest from '../components/baseTest'
 export default {
-  setup() {
+  props: ['xxx'],
+  setup(props) {
+    console.log('props is ', props)
     const baseTestCon = baseTest()
     console.log('baseTestCon is ', baseTestCon)
     return baseTestCon
